a66acf4c5ec56266724efbc3674ee2a4cb6c4248
Grekz <Grekz@users.noreply.github.com>
2024-02-13 00:13:23 +0100

    #2 - Add spring-boot example of simple crud

---
+++ spring-boot-java-crud-simple/src/main/java/com/grekz/crud/CrudJavaExampleApplication.java
@@ -0,0 +1,13 @@
+package com.grekz.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudJavaExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudJavaExampleApplication.class, args);
+	}
+
+}
---
+++ spring-boot-java-crud-simple/src/test/java/com/grekz/crud/CrudJavaExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grekz.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudJavaExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
